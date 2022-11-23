dd6e3f984a18740f0c4f93353c50411ecb300578
John Duimovich <jduimovich@gmail.com>
2022-11-23 09:28:11 -0500

    new totals cross groups
    - includes one generate file stats/aqastats-aarch64_linux-11.md

--- compute.sh
+++ compute.sh
@@ -10,19 +10,39 @@ fi
 # PLATFORM list for 11+: x86-64_linux x86-64_windows x86-64_mac x86-32_windows aarch64_linux ppc64le_linux ppc64_aix arm_linux x86-64_alpine-linux s390x_linux aarch64_mac
 if [ -z $PLATFORM ]; then
     PLATFORM="x86-64_linux"
-fi
+fi 
 
-mkdir -p data 
-OUT=stats/aqastats-${PLATFORM}-${VERSION}.md
-echo "## Platform: ${PLATFORM} Version: ${VERSION} " > $OUT
+mkdir -p data  
+#get all files 
 for group in system openjdk perf functional external
 do
-  echo "getTestAvgDuration for: $VERSION $PLATFORM "
+  echo "getTestAvgDuration for: $group on version $VERSION $PLATFORM "
   TEST_INFO=data/aqa${VERSION}-${group}.json
   curl -s -X 'GET' \
     "https://trss.adoptium.net/api/getTestAvgDuration?jdkVersion=$VERSION&platform=$PLATFORM&group=$group" \
-    -H 'accept: application/json' > $TEST_INFO
-  node format.js $TEST_INFO ${group} | tee -a $OUT
+    -H 'accept: application/json' > $TEST_INFO  
+done
+ 
+# Compute Totals for all groups
+TOTAL_TIME=0
+for group in system openjdk perf functional external
+do 
+  TEST_INFO=data/aqa${VERSION}-${group}.json 
+  GROUPTIME=$(cat $TEST_INFO | jq ".testListTime[0]")
+  TOTAL_TIME=$(echo $TOTAL_TIME+$GROUPTIME | bc) 
+done 
+TOTAL_TIME_HRS=$(echo "scale=2; $TOTAL_TIME / 1000 / 60 / 60" | bc)
 
+echo "TOTAL_TIME_HRS: $TOTAL_TIME_HRS TOTAL_TIME: $TOTAL_TIME"
+
+OUT=stats/aqastats-${PLATFORM}-${VERSION}.md
+echo "## Platform: ${PLATFORM} Version: ${VERSION} " > $OUT
+echo "### Total time across all groups $TOTAL_TIME_HRS hrs " >> $OUT
+echo "---" >> $OUT
+echo 
+for group in system openjdk perf functional external
+do 
+  TEST_INFO=data/aqa${VERSION}-${group}.json 
+  node format.js $TEST_INFO ${group} | tee -a $OUT 
 done
 
