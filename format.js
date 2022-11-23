588baa4bba6a0225adabeaec7af8790d79bc47ac
Shelley Lambert <slambert@gmail.com>
2022-11-22 22:05:08 -0500

    Add target totals
    Signed-off-by: Shelley Lambert <slambert@gmail.com>

--- format.js
+++ format.js
@@ -4,14 +4,23 @@ const group = myArgs[1];
 function run() {
     var stats = JSON.parse(fs.readFileSync(myArgs[0]));
     var time=parseInt(stats.testListTime[0]) / 1000 / 60 / 60
+    var count=0
+    for (var i = 0; i < stats.testLists.length; i++) {
+        var list = stats.testLists[i]
+
+        for (var j = 0; j < list.length; j++) {
+            if (list[j].avgDuration)
+                count++
+        }
+    }
     console.log()
     console.log("### ", group);
-    console.log(" Total Execution Time: ", time.toFixed(2), " hours");
+    console.log(" Execution time for ", count, " test targets: ", time.toFixed(2), " hours");
     console.log("<details><summary>Test target details</summary>")
     console.log()
     for (var i = 0; i < stats.testLists.length; i++) {
         var list = stats.testLists[i]
-        console.log("| Test Name | Time |")
+        console.log("| Test Target Name | Time |")
         console.log("| --- | --- |")
         for (var j = 0; j < list.length; j++) {
             if (list[j].avgDuration)
